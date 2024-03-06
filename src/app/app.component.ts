14267ec2c6803932e7c228bcb7781bced963adc9
Fazal ul Rehman <fazal.rehman@techsembly.com>
2024-03-06 13:42:55 +0500

    test on vercel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { Firestore ,collection , collectionData } from '@angular/fire/firestore';
+
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'firetest';
+
+  firestore : Firestore = inject(Firestore);
+  items$: Observable<any[]>
+
+
+  title= "FIREBASE GET COLLECTIONS";
+
+  constructor(){
+    const aCollection = collection(this.firestore, 'model')
+    this.items$ = collectionData(aCollection)
+    
+    // Subscribe to the observable and log the data when it arrives
+    this.items$.subscribe(data => {
+      console.log("FROM FIREBASE DATA:", JSON.stringify(data, null, 2));
+    });
+
+  }
+   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"cms-products-287d7","appId":"1:865928551591:web:5fa7d3770ce9f5dc2a4f82","storageBucket":"cms-products-287d7.appspot.com","apiKey":"AIzaSyBmo1ZZknD-3l38jY9IywLBbCJv3F5IvTI","authDomain":"cms-products-287d7.firebaseapp.com","messagingSenderId":"865928551591"})),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ],
-  providers: [],
+  providers: [
+    provideClientHydration()
+  ],
   bootstrap: [AppComponent]
+  
 })
 export class AppModule { }
